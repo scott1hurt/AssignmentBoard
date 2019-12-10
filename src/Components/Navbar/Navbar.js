e78dff6399211ef97a9ffc1434e354a55cd97efc
joseramirezjimenez <jose.ramirez5019b@gmail.com>
2019-12-10 10:29:19 -0600

    Added a Tutoring dropdown menu styling it

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,7 +5,13 @@ class Navbar extends Component {
   render() {
     return (
       <div className="navbar">
-        <p className='navbar-header'>Assignment Board</p>
+        <p className='navbar-header' onClick={this.props.formToggle}>Assignment Board</p>
+        <div className='dropdown'>
+          <button className='dropbtn'>Tutoring</button>
+          <div className='dropdown-content'>
+            <a target='_blank' href='https://www.wku.edu/getready/tutoring_glasgow.php'>WKU</a>
+          </div>
+        </div>
         <button onClick={this.props.formToggle} className='navbar-button'>+</button>
       </div>
     )
